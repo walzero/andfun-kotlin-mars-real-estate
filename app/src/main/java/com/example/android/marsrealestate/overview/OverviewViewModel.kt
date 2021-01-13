a3bb88f0dcdd9a0ba39038b79cd9f0504c5a7f59
Walter Rezende <walter-zero@hotmail.com>
2021-01-12 23:40:22 -0300

    Added recyclerview Added binding adapter for recyclerview adapter Added false to itemClip in recyclerview Changed OverviewFragment to use its own bindign again Changed grid_view_item to use a variable instead of viewmodel Added adapter with DiffCallback and viewholder for items Added binding for new adapter

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -38,10 +38,10 @@ class OverviewViewModel : ViewModel() {
     val status: LiveData<String>
         get() = _status
 
-    private val _property = MutableLiveData<MarsProperty>()
+    private val _properties = MutableLiveData<List<MarsProperty>>()
 
-    val property: LiveData<MarsProperty>
-        get() = _property
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
@@ -59,7 +59,7 @@ class OverviewViewModel : ViewModel() {
                 val listResult = MarsApi.retrofitService.getProperties()
 
                 if(listResult.isNotEmpty())
-                    _property.value = listResult[0]
+                    _properties.value = listResult
 
             } catch (e: Exception) {
                 _status.value = "Failure: ${e.message}"
